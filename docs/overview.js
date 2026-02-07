9399f064e62fc641c4dda706c1cb523a4631aeae
Konstantin Makarchev <kostya27@gmail.com>
2026-02-08 01:36:58 +0300

    add missing to readme

--- docs/overview.js
+++ docs/overview.js
@@ -242,6 +242,7 @@ function overview_tab($results) {
       <span class="language-tag">Nim</span>
       <span class="language-tag">F#</span>
       <span class="language-tag">Dart</span>
+      <span class="language-tag">Python</span>
     </div>
     <p class="language-note">Languages like Python, Ruby, or PHP are intentionally excluded to maintain a focused comparison within a similar performance bracket.</p>
   </div>
